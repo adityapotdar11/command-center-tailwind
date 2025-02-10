963b0fc2cbfbf52d90ffbb5c0ed9ee8256b530c2
adityapotdar11 <aditya@webster.co.in>
2025-02-10 18:16:06 +0530

    React router setup

--- src/Pages/Homepage/index.js
+++ src/Pages/Homepage/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Layout from "../../Components/Layout";
+import { Link } from "react-router-dom";
 
 const Homepage = () => {
     return (
         <>
             <Layout>
-                <p>This is test</p>
+                <p>
+                    <Link to="/prod">This</Link> goes to product page
+                </p>
+                <p>This is homepage</p>
             </Layout>
         </>
     );
--- src/Pages/Product/index.js
+++ src/Pages/Product/index.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Layout from "../../Components/Layout";
+import { Link } from "react-router-dom";
 
 const Product = () => {
     return (
         <>
             <Layout>
+                <p>
+                    <Link to="/">This</Link> goes to home page
+                </p>
                 <p>This is product page</p>
             </Layout>
         </>
